9b0c92f4a3e06419334c50f6af24655ded658359
kohnakagawa <tsunekou1019@gmail.com>
2016-11-18 16:44:37 +0900

    インデント幅を４から２に変更。キャストスタイルをC++のものに直した。

--- kernel.cuh
+++ kernel.cuh
@@ -1,14 +1,14 @@
 #pragma once
 
 __device__ __forceinline__ double atomicAdd(double* address, double val) {
-    unsigned long long int* address_as_ull = (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(val + __longlong_as_double(assumed)));
-    } while (assumed != old);
-    return __longlong_as_double(old);
+  auto address_as_ull = reinterpret_cast<unsigned long long int*>(address);
+  unsigned long long int old = *address_as_ull, assumed;
+  do {
+    assumed = old;
+    old = atomicCAS(address_as_ull, assumed,
+                    __double_as_longlong(val + __longlong_as_double(assumed)));
+  } while (assumed != old);
+  return __longlong_as_double(old);
 }
 
 template <typename Vec, typename Dtype>
