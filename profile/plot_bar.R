bc8e5a83eb2cbbbf150d3aea21961128c5c1dea6
kohnakagawa <tsunekou1019@gmail.com>
2017-10-25 15:22:21 +0900

    グラフの凡例を統一

--- profile/plot_bar.R
+++ profile/plot_bar.R
@@ -1,11 +1,14 @@
 library(ggplot2)
 library(reshape2)
 
-## out.file <- "force_k40t_oacc_cuda.png"
-## out.file <- "force_p100_oacc_cuda.png"
-out.file <- "cpu_gpu_oacc.png"
+out.file <- "ch2_force_oacc_cuda.png"
+## out.file <- "ch2_cpu_gpu_oacc.png"
 
 data <- read.table("./profile.dat")
+
+colnames(data) <- c("K40t\nOpenACC", "K40t\nCUDA", "P100\nOpenACC", "P100\nCUDA")
+## colnames(data) <- c("E5-2680v3", "K40t\nOpenACC", "P100\nOpenACC")
+
 data <- cbind(impl = rownames(data), data)
 df <- melt(data)
 g <- ggplot(
@@ -23,9 +26,6 @@ g <- g + theme(text = element_text(size = 22)) # change text size
 g <- g + labs(fill="") # remove legend title
 g <- g + theme(axis.title.y = element_text(vjust = 0.28)) # 軸のラベル位置を調整
 g <- g + theme(axis.text = element_text(color = "black")) # axis textの色を灰色から黒色に変更
-
-## g <- g + ggtitle("Tesla K40t") # add graph title
-## g <- g + ggtitle("Tesla P100") # add graph title
 
 plot(g)
 ggsave(out.file)
